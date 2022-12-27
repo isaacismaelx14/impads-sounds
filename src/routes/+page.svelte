216d0417877a291cf1d157a2e2e08bda97b359b4
Isaac Martinez <isaac@minnekdigital.com>
2022-12-26 23:59:25 -0400

    style: use space instead of tabs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,56 +1,56 @@
 <script lang="ts">
-	import Pad from '../components/Pad.svelte';
-	import { onMount } from 'svelte';
-	import type { IAudioItem } from './page';
-	
-	let items: IAudioItem[] = [];
+    import Pad from '../components/Pad.svelte';
+    import { onMount } from 'svelte';
+    import type { IAudioItem } from './page';
 
-	onMount(() => {
-		const input = document.querySelector<HTMLInputElement>('#audioInput');
-		if (!input) return;
+    let items: IAudioItem[] = [];
 
-		input.addEventListener('change', () => {
-			if (!input.files?.length) return;
-			const { name } = input.files[0];
-			const id = `${name.replaceAll(/[^a-zA-Z0-9]/g, '-')}-${Date.now()}`;
-			const src = URL.createObjectURL(input.files[0]);
+    onMount(() => {
+        const input = document.querySelector<HTMLInputElement>('#audioInput');
+        if (!input) return;
 
-			items.push({ id, src, name });
-			items = [...items];
-		});
-	});
+        input.addEventListener('change', () => {
+            if (!input.files?.length) return;
+            const { name } = input.files[0];
+            const id = `${name.replaceAll(/[^a-zA-Z0-9]/g, '-')}-${Date.now()}`;
+            const src = URL.createObjectURL(input.files[0]);
+
+            items.push({ id, src, name });
+            items = [...items];
+        });
+    });
 </script>
 
 <svelte:head>
-	<title>I'm all hears</title>
+    <title>I'm all hears</title>
 </svelte:head>
 
 <section>
-	<input type="file" accept="audio/*" id="audioInput" />
+    <input type="file" accept="audio/*" id="audioInput" />
 
-	<ul class="leads">
-		{#each items as item}
-			<li>
-				<Pad {item} />
-			</li>
-		{/each}
-	</ul>
+    <ul class="leads">
+        {#each items as item}
+            <li>
+                <Pad {item} />
+            </li>
+        {/each}
+    </ul>
 </section>
 
 <style>
-	.leads {
-		display: grid;
-		gap: 1rem;
-		grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
-		list-style-type: none;
-		margin: 0 auto;
-		margin-block: 50px;
-		max-width: 700px;
-		padding: 0;
-	}
+    .leads {
+        display: grid;
+        gap: 1rem;
+        grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+        list-style-type: none;
+        margin: 0 auto;
+        margin-block: 50px;
+        max-width: 700px;
+        padding: 0;
+    }
 
-	.leads li {
-		width: 100%;
-		height: 120px;
-	}
+    .leads li {
+        width: 100%;
+        height: 120px;
+    }
 </style>
